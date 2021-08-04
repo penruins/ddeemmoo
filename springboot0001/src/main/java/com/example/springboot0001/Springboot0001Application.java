a82c37c31edfcafde15a89d883bd1bd170d443d2
penruins <lx_tdcq_king@icloud.com>
2021-08-05 00:14:36 +0800

    commit

---
+++ springboot0001/src/main/java/com/example/springboot0001/Springboot0001Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot0001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0001Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Springboot0001Application.class, args);
+  }
+
+}
---
+++ springboot0001/src/test/java/com/example/springboot0001/Springboot0001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot0001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot0001ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
